e2935abb666084cc1cb253b63880a4a0ea82f770
Anton Nikulin <dr.tallin@gmail.com>
2014-04-28 16:30:27 +0300

    server: fixed: json attribute error uncaught exception

--- server/handlers/WebSocketGameHandler.py
+++ server/handlers/WebSocketGameHandler.py
@@ -15,11 +15,16 @@ class WebSocketGameHandler(tornado.websocket.WebSocketHandler):
 
     def on_message(self, message):
         logging.warning("Message Received: "+message)
+        messageCode = None
         try:
             parsedMessage = json.loads(message)
             messageCode = parsedMessage.get('type', None)
+        except AttributeError:
+            logging.error("wsHandler on_message AttributeError. Message: "+message)
+            self.write_message(json.dumps({"type":"ERROR", "description":"JSON AttributeError.", "message":message}))
         except ValueError:
             logging.error("wsHandler on_message Val error: "+message)
+
         if messageCode == MessageType.GetCard:
             self.write_message(json.dumps({"type":"cardReceived","card":
                                                                     {"name":"Stormtrooper","health":52, "attack":72}}))
