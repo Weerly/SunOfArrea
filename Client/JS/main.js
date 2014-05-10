d2fa1ce64eedbfb3a900a1548918c29324cf5b2e
Weerly <weerly3@gmail.com>
2014-05-10 17:21:13 +0300

    create  room and connect to room,PreBattle room

--- Client/JS/main.js
+++ Client/JS/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){ 
     socket.init("ws://localhost:8000/ws");
+    var selectarray = [];
+    if(selectarray ===null){
+        $('#Enter').attr('disabled','true');
+    }else{$('#Enter').attr('disabled','false');}
    $('#login').click(function(){
    var username = $('#inputLogin').val();
    var password = $('#inputPassword').val();
@@ -15,20 +19,55 @@ $(document).ready(function(){
     dataType: "html",
     success:function(data){
         $(document).attr({title:'Rooms'});
-        socket.sendMessage({'type' :10});
+        socket.sendMessage({'type' :constants.type("GetListOfRoom")});
         $('body').html(data);
     }
     });
    });
-   var selectarray = [];
-   $(document).on('click','#refresh',function(){socket.sendMessage({'type' :10});});
-   $(document).on('click','#Create',function(){socket.sendMessage({'type' :20});});
-   $(document).on('click','#Enter',function(){socket.sendMessage({'type' :10});});
-   $(document).on('dblclick','.roomlist',function(){socket.sendMessage({'type' :10});});
+   $(document).on('click','#Enter',function(){if(selectarray.length !==0){  
+           socket.sendMessage({'type' :constants.type("ConnectToRoom"),'id':selectarray[0]});}});
+   $(document).on('click','#Create',function(){socket.sendMessage({'type' :constants.type("CreateRoom"),'name':'battle'});});
+   $(document).on('click','#refresh',function(){socket.sendMessage({'type' :constants.type("GetListOfRoom")});});
+   $(document).on('dblclick','.roomlist',function(){if(selectarray.length !==0){  
+           socket.sendMessage({'type' :constants.type("ConnectToRoom"),'id':selectarray[0]});
+   }});
    $(document).on('mouseover','.roomlist',function(){
       $(this).css({cursor: "pointer"}); 
    });
    $(document).on('click','.roomlist',function(){
        Room.select($(this),selectarray);
+   });
+   $(document).on('mouseover','#HeroClass',function(){
+      Card.overAnimation($(this)); 
+   });
+   $(document).on('mouseleave','#HeroClass',function(){
+      Card.leaveAnimation($(this)); 
+   });
+   $(document).on('click','#HeroClass',function(){
+       var Cards={"type":"cardReceived","class":"HeroClass","cardType":[{"1":"Паладин","2":"Маг","3":"Разбойник","4":"Лучник"}]};
+       var Cardss = JSON.stringify(Cards);
+       receivedData.filter(Cardss);
+   });
+   $(document).on('mouseover','#Skills',function(){
+      Card.overAnimation($(this)); 
+   });
+   $(document).on('mouseleave','#Skills',function(){
+      Card.leaveAnimation($(this)); 
+   });
+   $(document).on('click','#Skills',function(){
+       //var Cards=["Разящий клинок","Кромсающий выпад","Берсерк","Оглушение щитом","Укол","Железная стрела"];
+       //console.log(Cards);
+       //Card.clickAnimation($(this,Cards));
+       socket.sendMessage({'type' :1});
+   });
+   $(document).on('mouseover','#Weapons',function(){
+      Card.overAnimation($(this)); 
+   });
+   $(document).on('mouseleave','#Weapons',function(){
+      Card.leaveAnimation($(this)); 
+   });
+   $(document).on('click','#Weapons',function(){
+       var Cards=["Меч одноручный","Меч двуручный","2 меча одноручных","Щит","Копье","Кинжал"];
+       Card.clickAnimation($(this,Cards));
    });
 });
--- Client/js/Objects.js
+++ Client/js/Objects.js
@@ -4,7 +4,13 @@ var receivedData = {
     },
     type: function(Data){
         switch(Data.type){
-            case"listOfRooms": Room.display(Data.rooms);
+            case 11: Room.display(Data.rooms);
+                break;
+             case 31:Room.connect(Data);
+                break;
+              case 32:Room.afterCreate(Data);
+                break;
+            case 21: Room.create(Data);
                 break;
             case"cardReceived": Card.display(Data.card);
                 break;
@@ -13,16 +19,48 @@ var receivedData = {
         };
     }
 };
+var constants = {
+    type: function(type){
+        var constant;
+        switch(type){
+            case'GetListOfRoom':
+                constant = 10;
+                break;
+            case'ListOfRooms':
+                constant = 11;
+                break;
+            case'CreateRoom':
+                constant = 20;
+                break;
+            case'RoomCreated':
+                constant = 21;
+                break;
+            case'ConnectToRoom':
+                constant = 30;
+                break;
+            case'ConnectedToRoom':
+                constant = 31;
+                break;
+            case'PlayerConnected':
+                constant = 32;
+                break;   
+        }
+        return constant;
+    }
+};
 var Room = {
     display:function(room){
         console.log(room);
         this.roomList(room);
     },
     create: function(Data){
-        console.log(Data);
+        Battle.prepare(Data);
     },
-    join: function(Data){
-        
+    afterCreate: function(Data){
+        Battle.playerConnected(Data);
+    },
+    connect: function(Data){
+        Battle.prepare(Data);
     },
     roomList:function(room){
         $('#roomcontent').html('');
@@ -30,38 +68,63 @@ var Room = {
         this.printRoom(room);
     },
     printRoom:function(room){
-        var divBody;
+        var divLeft;
+        var emptyRoom = '';
         if(room.length===0){
-            divBody = 'Нету пустых комнат, может подождать пока,'+
-                    'кто-то создаст комнату или созадйте сами';
+            divLeft = '<div>Нету пустых комнат, можете подождать пока,'+
+                    'кто-то <br/>создаст комнату или создайте сами! Для обновления списка нажмите кнопку "Обновить"</div>';
+            emptyRoom = 'emptyRoomList';
             room.length++;
+            var divRight = '<div></div>';
+        }
+        var divAttribute = '<div class="roomlist '+emptyRoom+'">';
+        this.printRoomCycle(room,emptyRoom,divAttribute,divLeft,divRight);
+        if(room.length>8){
+            $('#roomcontent').css({"overflow-y":"scroll"}); 
+         }    
+    },
+    printRoomCycle:function(room,emptyRoom,divAttribute,divLeft,divRight){
+        var enable = true;
+        if(enable === true){
+            enable = 'enable';
+        }else{
+            enable = 'disable';
         }
         for(var i=0;i<room.length;i++){
-            var divAttribute = '<div class="roomlist" id="'+i+'">';
-            if(room.length!==0){
-                divBody = room[i].player1+'комната №'+(i+1);
+            if(emptyRoom === ''){
+                console.log(room.name);
+                var divAttribute = '<div class="roomlist" id='+room[i].id+'>';
+                var divAttributeLeft = '<div class="roomlistblock centered '+enable+' f-right">';
+                var divAttributeRight = '<div class="roomlistblock centered '+enable+' f-left">';
+                var divBodyLeft = room[i].player1;
+                var divBodyRight = 'комната №'+(i+1);
+                $('.room').css("height", "+=28px");
+                divLeft = divAttributeLeft+divBodyLeft+'</div>';
+                divRight = divAttributeRight+divBodyRight+'</div>';
+            }else{ 
+                $('.room').css("height", "+=60px");
             }
-            var roomInList = divAttribute+divBody+'</div>';
+            var roomInList = divAttribute+divLeft+divRight+'</div>';
             $('#roomcontent').append(roomInList);
-            $('.room').css("height", "+=28px");
-            if(room.length>8){
-               $('#roomcontent').css({"overflow-y":"scroll"}); 
-            }
-        }    
-        
+        }
     },
     select:function($this,selectArray){
         var Id = $this.attr('id');
+        alert(Id);
         $id = '#'+Id+'';
         if(selectArray.length === 0){
                selectArray.push(Id,$id);
-               $this.css({'background':'violet'});
+               $this.css({'background':'linear-gradient(315deg, green 10%,rgba(48,127,255,0.1) 20%, green 30%,'+
+                           'rgba(48,127,255,0.1) 40%, green 50%,rgba(48,127,255,0.1) 60%,'+
+                           ' green 70%,rgba(48,127,255,0.1) 80%, green 90%,rgba(48,127,255,0.1) 100%)'});
            }else if(Id !== selectArray[0]){
                $(selectArray[1]).css({'background':'none'});
                selectArray.pop(selectArray[0]);
                selectArray.pop(selectArray[1]);
                selectArray.push(Id,$id);
-               $this.css({'background':'violet'});
+               $this.css({'background':'linear-gradient(315deg, green 10%,rgba(48,127,255,0.1) 20%, green 30%,'+
+                           'rgba(48,127,255,0.1) 40%, green 50%,rgba(48,127,255,0.1) 60%,'+
+                           ' green 70%,rgba(48,127,255,0.1) 80%, green 90%,rgba(48,127,255,0.1) 100%)'});
 
            }else{
                $(selectArray[1]).css({'background':'none'});
@@ -70,6 +133,122 @@ var Room = {
            }
     }
 };
+ var Battle = {
+     blurLoad:function(){
+        $('.blur-block').blurjs({
+		overlay: 'rgba(255,255,255,0.1)',
+		radius:7		
+	});
+    },
+     prepare: function(data){
+         $("body").load("/pages/BattlepreRoom.html",function(data){
+             Ajax.$success_("Room",data);
+             $('#1stPlayerName').text('Your Name');
+             $('#1stPlayerClass').text('Your Class');
+             Battle.blurLoad();
+         });
+     },
+     playerConnected:function(data){
+            $('#2ndPlayerName').text('Your Name');
+            $('#2ndPlayerClass').text('Your Class');
+     }
+ }; 
  var Card = {
-     display: function(card){console.log(card);}
+     display: function(cards){
+         this.clickAnimation(cards);
+     },
+     overAnimation:function($this){
+         var $next = $this.next();
+         var nextHeight = $this.height();
+         $this.css({"box-shadow":"0 0 200px green"});
+         $this.css({"height":"+=3%"});
+         $this.css({"width":"+=2%"});
+         $this.zIndex(5);        
+     },
+     Front:function(front,id){
+         if (front) {
+        //$(this).css('background', '#f0f');
+    } else {
+        $(this).css('background', '#0f0');
+        alert(id);
+    }
+     },
+     leaveAnimation:function($this){
+         var $next = $this.next();
+         var nextHeight = "1%";
+         $this.css({"box-shadow":"none"});
+         $this.css({"height":"-=3%"});
+         $this.css({"width":"-=2%"});
+         $this.zIndex(0);
+     },
+     clickAnimation:function(Cards,page){
+         var length = 0;
+         var id_ = "Skills";
+         //var d = count;
+         //var $this = $('#Skills');
+         var $this = $('#'+id_+'');
+         //console.log(Cards);
+         for(var i=0;i<4;i++){
+             
+             //console.log($this);
+             //console.log(i);
+             var percent = $(window).width()*10/100;
+             console.log($(window).width());
+             console.log(percent);
+            length +=percent;
+            var $next = $this.next();
+            var nextHeight = "1%";
+            $this.css({"box-shadow":"none"});
+            $this.zIndex(0);
+            $this.clone().prependTo($this.parent()).attr("id",id_+i);
+            //console.log($this.clone().attr("id"));
+            //console.log(length);
+            $this.delay(5000);
+            $this.animate({
+                left:300+length,
+                opacity:1
+            },2000);
+            $this.rotate3Di(180,800,{
+                sideChange: function(front){
+                    
+                        if (front) {
+                       //$(this).css('background', '#f0f');
+                        } else {  
+                            $(this).css('background', 'url("/img/'+Cards.name+'.jpg")');
+                            $(this).css('backgroundSize', '100% 100%');
+                            console.log($(this).css('background'));
+                        }
+                    }
+            });
+            
+            $this = $("#"+id_+i+"");
+            console.log($this);
+        }
+     }   
  };
+ var Ajax = {
+     $success_:function(type,data){
+         switch(type){
+             case'authorisation':   $(document).attr({title:'Rooms'});
+                                    socket.sendMessage({'type' :10});
+                                    $('body').html(data);
+                                    break;
+             case'Room':            $(document).attr({title:'Preparation To Battle'});
+                                    //socket.sendMessage({'type' :21});
+                                    $('body').html(data);
+                                    $('body').css({"background":"url('/img/Wood_Texture.jpg')"});
+                                    break;
+         };
+     },
+     $ajax:function(url,dataType,type){
+        $.ajax({
+        url: url,
+        dataType: dataType,
+        success:function(data){
+            Ajax.$success_(type,data);
+            }    
+        });
+    }
+};
+//<!--script type='text/javascript' src='http://code.jquery.com/jquery-1.7.1.js'></script>
+//	<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js'></script-->
