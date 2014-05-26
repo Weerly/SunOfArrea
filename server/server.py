f782da003ef989a5eddd1031a0cf52389262b13d
Anton Nikulin <dr.tallin@gmail.com>
2014-05-26 16:42:16 +0300

    add: print port on which server will run

--- server/server.py
+++ server/server.py
@@ -2,10 +2,16 @@ import logging
 
 import tornado.httpserver
 import tornado.ioloop
-import tornado.options
 import tornado.web
+from tornado.options import define, options
 
 from app.gameApp import SoAGameServer
+
+define("port", default=8000, help="run on the given port", type=int)
+
+message = """------------SOA gameServer--------------
+port:{0}
+"""
 
 if __name__ == "__main__":
     #Set log level
@@ -14,5 +20,7 @@ if __name__ == "__main__":
     #Tornado config
     soaApp = SoAGameServer()
     http_server = tornado.httpserver.HTTPServer(soaApp)
-    http_server.listen(8000)
+    http_server.listen(options.port)
+    print message.format(options.port)
     tornado.ioloop.IOLoop.instance().start()
+
